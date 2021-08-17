50ebc4e13f4dffb79e782e7cf163c65e27c51548
easy-lE <86132543+easy-lE@users.noreply.github.com>
2021-08-17 16:40:37 -0500

    17_agosto

---
+++ Ejemplos_en_C/bin/hi_mundo.c
@@ -0,0 +1,9 @@
+/* 
+  Nombre: Erick Martínez Flores
+  Fecha: 05 de agosto de 2021
+*/
+#include <stdio.h>
+int main(){
+    printf("Hola erick");
+    return(0);
+}
---
+++ Ejemplos_en_C/bin/hola_mundo.c
@@ -0,0 +1,12 @@
+/* Primer programa en Visual Studio Code
+  Nombre: Erick Martínez Flores
+  Fehca: 05 de agosto de 2021
+*/
+
+#include <stdio.h>
+
+
+int main(){
+    printf("Hola UAM");
+    return(0);
+}
